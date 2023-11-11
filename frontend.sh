186a1b28db0de4e4ab198240ec5456a684d604dc
SRINU <vasu_anand01@yahoo.co.in>
2023-11-11 08:57:46 +0530

    frontend set up

--- frontend.sh
+++ frontend.sh
@@ -3,18 +3,24 @@ echo -e "\e[35m instal Nginx \e[0m"
 dnf install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[35m remove Frontend  \e[0m"
-rm -f /usr/share/nginx/html/*
+rm -f /usr/share/nginx/html/* &>>/tmp/roboshop.log
+
 
 echo -e "\e[35m Dowloading frontend  \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
+
 
 echo -e "\e[35m Extractin & Copy Frontend  \e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[35m Update Frontend Configuration \e[0m"
-cp /home/centos/project-roboshell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35m Update Frontend Configuration \e[0m" 
+cp /home/centos/project-roboshell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
+
 
 echo -e "\e[35m Enable SystemD \e[0m"
-systemctl enable nginx 
-systemctl restart nginx 
+systemctl enable nginx &>>/tmp/roboshop.log
+
+systemctl restart nginx &>>/tmp/roboshop.log
+
