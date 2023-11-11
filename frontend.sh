973eb2543d7c83cd72b53faf7fd69f700cfc4741
SRINU <vasu_anand01@yahoo.co.in>
2023-11-11 22:31:14 +0530

    8

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,15 @@ echo -e "\e[35m instal Nginx \e[0m"
 dnf install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[35m remove Frontend  \e[0m"
-rm -f /usr/share/nginx/html/* &>>/tmp/roboshop.log
+rm -f /usr/share/nginx/html/* 
+stat_check() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    exit 1
+  fi
+}
 
 
 echo -e "\e[35m Dowloading frontend  \e[0m"
